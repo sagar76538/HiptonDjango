e0d172071d6b61ab36ed6f3d5a0e5aa97c74e527
HarshSingh-01 <harshkushwah2011@gmail.com>
2023-09-04 02:02:07 +0530

    functionality has been added for category wise filter and product page landing

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'image_tag' )
+
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Categories)
--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home"),
+    path("", views.index, name="index"),
+    # path("home", views.index, name="home"),
     path("product/<int:id>/", views.get_product, name="product"),
 ]
 
